b08859a7e04c3b6d6aef1ec2b5d8ad96d35dd86f
gnizdyuh <ekstrumal@ukr.net>
2020-05-20 16:23:40 +0300

    fix websocket

--- src/api/SocketApi.js
+++ src/api/SocketApi.js
@@ -2,10 +2,11 @@ import socket from 'socket.io-client';
 
 class SocketApi {
   init(token) {
-    this.socket = socket('/', {
+    this.socket = socket('https://apiko-intensive-backend.herokuapp.com/', {
       query: {
         token
-      }
+      },
+      transports: [ 'websocket' ]
     });
 
     this.socket.on('connect', () => {
@@ -14,6 +15,7 @@ class SocketApi {
   }
 
   handleMessages(handler) {
+    this.socket.on('message', (message) => console.log(JSON.parse(message)));
     this.socket.on('message', (message) => handler(JSON.parse(message)));
   }
 }
